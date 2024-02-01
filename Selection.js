689a897f5222a8a7d900b1ff50ea5ff43f18a3bd
Anmay Srivastava <103562464+anmay12sri@users.noreply.github.com>
2024-02-01 22:51:55 +0530

    Update Selection.js

--- Selection.js
+++ Selection.js
@@ -1,136 +1,81 @@
 const container = document.querySelector(".data-container");
 
-// function to generate bars
-function generatebars(num = 20) {
-
-  
-  for (let i = 0; i < num; i ++) {
-
-    // To generate random values from 1 to 100
+function generateBars(num = 20) {
+  for (let i = 0; i < num; i++) {
     const value = Math.floor(Math.random() * 100) + 1;
-
-  
     const bar = document.createElement("div");
-
-    
     bar.classList.add("bar");
-
-    
     bar.style.height = `${value * 3}px`;
-
-    // Translate the bar towards positive X axis
     bar.style.transform = `translateX(${i * 30}px)`;
-
-    // To create element "label"
     const barLabel = document.createElement("label");
-
-    // To add class "bar_id" to "label"
     barLabel.classList.add("bar_id");
-
-    // Assign value to "label"
     barLabel.innerHTML = value;
-
-    // Append "Label" to "div"
     bar.appendChild(barLabel);
-
-    // Append "div" to "data-container div"
     container.appendChild(bar);
   }
 }
 
-// asynchronous function to perform "Selection Sort"
-async function SelectionSort(delay = 300) {
+async function selectionSort(delay = 300) {
   let bars = document.querySelectorAll(".bar");
-  // Assign 0 to min_idx
-   var min_idx = 0;
-   for (var i = 0; i < bars.length; i += 1) {
+  const totalBars = bars.length;
+  let min_idx = 0;
 
-    // Assign i to min_idx
+  for (let i = 0; i < totalBars; i++) {
     min_idx = i;
 
-    // Provide darkblue color to the ith bar
     bars[i].style.backgroundColor = "darkblue";
-    for (var j = i + 1; j < bars.length; j += 1) {
 
-      // Provide red color to the jth bar
+    for (let j = i + 1; j < totalBars; j++) {
       bars[j].style.backgroundColor = "red";
 
-      // To pause the execution of code for 300 milliseconds
-      await new Promise((resolve) =>
-        setTimeout(() => {
-          resolve();
-        }, 300)
-      );
+      await new Promise((resolve) => setTimeout(resolve, delay));
 
-      // To store the integer value of jth bar to var1
-      var val1 = parseInt(bars[j].childNodes[0].innerHTML);
+      const val1 = parseInt(bars[j].childNodes[0].innerHTML);
+      const val2 = parseInt(bars[min_idx].childNodes[0].innerHTML);
 
-      // To store the integer value of (min_idx)th bar to var2
-      var val2 = parseInt(bars[min_idx].childNodes[0].innerHTML);
-
-      // Compare val1 & val2
       if (val1 < val2) {
         if (min_idx !== i) {
-
-          // Provide skyblue color to the (min-idx)th bar
-          bars[min_idx].style.backgroundColor = "  rgb(24, 190, 255)";
+          bars[min_idx].style.backgroundColor = "rgb(24, 190, 255)";
         }
         min_idx = j;
       } else {
-
-        // Provide skyblue color to the jth bar
-        bars[j].style.backgroundColor = "  rgb(24, 190, 255)";
+        bars[j].style.backgroundColor = "rgb(24, 190, 255)";
       }
     }
 
-    // To swap ith and (min_idx)th bar
-    var temp1 = bars[min_idx].style.height;
-    var temp2 = bars[min_idx].childNodes[0].innerText;
-    bars[min_idx].style.height = bars[i].style.height;
-    bars[i].style.height = temp1;
-    bars[min_idx].childNodes[0].innerText = bars[i].childNodes[0].innerText;
-    bars[i].childNodes[0].innerText = temp2;
+    [bars[min_idx].style.height, bars[i].style.height] = [bars[i].style.height, bars[min_idx].style.height];
+    [bars[min_idx].childNodes[0].innerText, bars[i].childNodes[0].innerText] = [bars[i].childNodes[0].innerText, bars[min_idx].childNodes[0].innerText];
 
-    // To pause the execution of code for 300 milliseconds
-    await new Promise((resolve) =>
-      setTimeout(() => {
-        resolve();
-      }, 300)
-    );
+    await new Promise((resolve) => setTimeout(resolve, delay));
 
-    // Provide skyblue color to the (min-idx)th bar
-    bars[min_idx].style.backgroundColor = "  rgb(24, 190, 255)";
-
-    // Provide lightgreen color to the ith bar
-    bars[i].style.backgroundColor = " rgb(49, 226, 13)";
+    bars[min_idx].style.backgroundColor = "rgb(24, 190, 255)";
+    bars[i].style.backgroundColor = "rgb(49, 226, 13)";
   }
 
-  // To enable the button "Generate New Array" after final(sorted)
+ 
   document.getElementById("Button1").disabled = false;
   document.getElementById("Button1").style.backgroundColor = "#6f459e";
 
-  // To enable the button "Selection Sort" after final(sorted)
   document.getElementById("Button2").disabled = false;
   document.getElementById("Button2").style.backgroundColor = "#6f459e";
 }
 
-// Call "generatebars" function
-generatebars();
+function generate() {
+  // Clear the container and generate new bars
+  container.innerHTML = "";
+  generateBars();
+}
 
-// function to generate new random array
- function generate()
-{
-  window.location.reload();
- }
-
-//  function to disable the button
-function disable()
-{
-  // To disable the button "Generate New Array"
+function disable() {
   document.getElementById("Button1").disabled = true;
   document.getElementById("Button1").style.backgroundColor = "#d8b6ff";
-
-  // To disable the button "Selection Sort"
   document.getElementById("Button2").disabled = true;
   document.getElementById("Button2").style.backgroundColor = "#d8b6ff";
 }
+
+function startSort() {
+  disable();
+  selectionSort();
+}
+
+generateBars();
